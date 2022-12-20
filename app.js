9ac49d523e6da3c176a5fab1e152665622d494c0
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-20 18:53:56 +0530

    build in modules

--- app.js
+++ app.js
@@ -7,6 +7,31 @@
 // process -> info about env where program is being executed
 
 // Modules -> encapsulted code;
-const names = require("./names"); // USE REQUIRE TO GET EXPORTED STUFF
-const sayHello = require("./uitls/sayHi");
-sayHello(names.n1);
+// const names = require("./names"); // USE REQUIRE TO GET EXPORTED STUFF
+// const sayHello = require("./uitls/sayHi");
+// sayHello(names.n1);
+
+// BUILD IN MODULES
+
+// 1) OS module
+
+// const os = require("os");
+
+// info curr user
+// const user = os.userInfo();
+// console.log(user);
+
+// SYSTEM UP-TIME
+// console.log(os.uptime());
+
+// 2) PATH module
+
+// const path = require("path");
+// console.log(path.sep);
+// const abspath = path.resolve(__dirname, "utils", "sayHi.js");
+// console.log(abspath);
+// PATH STUFF
+
+// 3) FS (SYNC)
+
+const { readFileSync, writeFileSync } = require("fs");
