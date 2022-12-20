d61c5c50d233ab863145e3bae0df3d957035f26c
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-20 18:10:00 +0530

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+// GLOBALS - NO WINDOW !!
+
+// __dirname -> path to curr directory
+// __filename -> file name
+// require -> function to use modules (common Js)
+// module -> info about curr module
+// process -> info about env where program is being executed
