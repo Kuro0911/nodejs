5dc285ed30f9d7b9704064477c1c360f73d2910a
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-20 19:39:52 +0530

    fs + npm

--- app.js
+++ app.js
@@ -34,16 +34,65 @@
 
 // 3) FS (SYNC)
 
-const { readFileSync, writeFileSync } = require("fs");
+// const { readFileSync, writeFileSync } = require("fs");
 
-const first = readFileSync("./content/first.txt", "utf-8");
-const second = readFileSync("./content/second.txt", "utf-8");
+// const first = readFileSync("./content/first.txt", "utf-8");
+// const second = readFileSync("./content/second.txt", "utf-8");
 
-console.log(first);
-console.log(second);
+// console.log(first);
+// console.log(second);
 
-writeFileSync(
-  "./content/three.txt",
-  "this is the third txt file made using the write file sync",
-  "utf-8"
-);
+// writeFileSync(
+//   "./content/three.txt",
+//   "this is the third txt file made using the write file sync",
+//   "utf-8"
+// );
+
+// 3) FS (ASYNC)
+
+// const { readFile, writeFile } = require("fs");
+
+// readFile("./content/first.txt", "utf-8", (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     const first = res;
+//     readFile("./content/second.txt", "utf-8", (err, res) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         const second = res;
+//         writeFile(
+//           "./content/async-result",
+//           `data : ${first} and ${second}`,
+//           (err, res) => {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               console.log("ok");
+//             }
+//           }
+//         );
+//       }
+//     });
+//   }
+// });
+
+// 4) HTTP MODULE
+// basic server
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("hello world");
+//   } else if (req.url === "/about") {
+//     res.write("about section");
+//   } else {
+//     res.write(
+//       `<h1>Oops!</h1> <h1>The Page you are Looking for does not exist!!</h1><a href="/">back to slums </a>`
+//     );
+//   }
+//   res.end();
+// });
+
+// server.listen(5000);
