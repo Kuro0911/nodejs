fa59b2ef2373d1e72a67cb3fb62e9f119d28b6b7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-26 17:02:42 +0530

    db setup

--- controlers/tasks.js
+++ controlers/tasks.js
@@ -1,17 +1,56 @@
-const getAllTasks = (req, res) => {
-  res.send("all itms");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const getSingleTasks = (req, res) => {
-  res.send("Single itms");
+const getSingleTasks = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    if (!task) {
+      return res.status(404).json({ msg: "task does not exists" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const updateSingleTask = (req, res) => {
-  res.send("update itms");
+const updateSingleTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: "task does not exist" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const delSingleTask = (req, res) => {
-  res.send("del itms");
+const delSingleTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id });
+    if (!task) {
+      return res.status(404).json({ msg: "task does not exists" });
+    }
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
